237069a8c2e837d2a842c2b0bbae7db24cebc70a
Vladimir Kishlaly <kishlaly@devexperts.com>
2017-05-06 22:24:19 +0300

    remove blank lines

--- u.js
+++ u.js
@@ -4,28 +4,21 @@
 *  http://www.webtoolkit.info/
 *
 **/
-
 var Url = {
-
 	// public method for url encoding
 	encode : function (string) {
 		return escape(this._utf8_encode(string));
 	},
-
 	// public method for url decoding
 	decode : function (string) {
 		return this._utf8_decode(unescape(string));
 	},
-
 	// private method for UTF-8 encoding
 	_utf8_encode : function (string) {
 		string = string.replace(/\r\n/g,"\n");
 		var utftext = "";
-
 		for (var n = 0; n < string.length; n++) {
-
 			var c = string.charCodeAt(n);
-
 			if (c < 128) {
 				utftext += String.fromCharCode(c);
 			}
@@ -38,22 +31,16 @@ var Url = {
 				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
 				utftext += String.fromCharCode((c & 63) | 128);
 			}
-
 		}
-
 		return utftext;
 	},
-
 	// private method for UTF-8 decoding
 	_utf8_decode : function (utftext) {
 		var string = "";
 		var i = 0;
 		var c = c1 = c2 = 0;
-
 		while ( i < utftext.length ) {
-
 			c = utftext.charCodeAt(i);
-
 			if (c < 128) {
 				string += String.fromCharCode(c);
 				i++;
@@ -69,10 +56,7 @@ var Url = {
 				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
 				i += 3;
 			}
-
 		}
-
 		return string;
 	}
-
 };
